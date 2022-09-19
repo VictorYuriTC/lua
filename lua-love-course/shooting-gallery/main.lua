62e4d5e6e3a137b981b67dd7b10610061f1ff05a
Victor Yuri <victoryuritc@yahoo.com>
2022-09-19 19:31:14 -0300

    msgs to give user info. mini-project completed

--- lua-love-course/shooting-gallery/main.lua
+++ lua-love-course/shooting-gallery/main.lua
@@ -5,7 +5,8 @@ function love.load()
   target.y = math.random(love.graphics.getHeight())
 
   score = 0
-  timer = 10
+  timer = 0
+  gameState = 1
 
   gameFont = love.graphics.newFont(40)
 
@@ -24,6 +25,7 @@ function love.update(dt)
 
   if timer < 0 then
     timer = 0
+    gameState = 1
   end
 end
 
@@ -32,29 +34,38 @@ function love.draw()
   love.graphics.draw(sprites.sky, 0, 0)
 
   love.graphics.setColor(1, 1, 1)
-  love.graphics.print(score, 0, 0)
+  love.graphics.print('Score: ' ..score, 5, 5)
   love.graphics.setFont(gameFont)
 
-  love.graphics.print(math.ceil(timer), 300, 0)
+  love.graphics.print('Time: ' ..math.ceil(timer), love.graphics.getWidth() / 2 - 70, 5)
 
-  love.graphics.setColor(1, 0, 0)
-  love.graphics.circle("fill", target.x, target.y, 50)
-  
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
-  
+  if gameState == 2 then
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(sprites.target, target.x - target.radius, target.y - target.radius)
+  end
+
   love.graphics.setColor(0, 1, 0)
   love.graphics.draw(sprites.crosshairs, love.mouse.getX() - 20, love.mouse.getY() - 20)
+
+  if gameState == 1 then
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.printf("Click anywhere to begin!", 0, 250, love.graphics.getWidth(), 'center')
+  end
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-  if button == 1 then
+
+  if button == 1 and gameState == 2 then
     local mouseToTarget = distanceBetween(x, y, target.x, target.y)
     if mouseToTarget < target.radius then
       score = score + 1
       target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
       target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
     end
+  elseif button == 1 and gameState == 1 then
+    gameState = 2
+    timer = 10
+    score = 0
   end
 end
 
