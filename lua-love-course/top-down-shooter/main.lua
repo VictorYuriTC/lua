79190dbbc5c7fe9157b36346b2ae30cfcb20df36
Victor Yuri <victoryuritc@yahoo.com>
2022-09-19 19:56:34 -0300

    initial commits with sprites

---
+++ lua-love-course/top-down-shooter/main.lua
@@ -0,0 +1,15 @@
+function love.load()
+  sprites = {}
+  sprites.background = love.graphics.newImage('sprites/background.png')
+  sprites.bullet = love.graphics.newImage('sprites/bullet.png')
+  sprites.player = love.graphics.newImage('sprites/player.png')
+  sprites.zombie = love.graphics.newImage('sprites/zombie.png')
+end
+
+function love.update()
+
+end
+
+function love.draw()
+  love.graphics.draw(sprites.background, 0, 0)
+end
