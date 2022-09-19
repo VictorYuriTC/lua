c54ee3a686c0620f9b882e8fa827e8981475f66b
Victor Yuri <victoryuritc@yahoo.com>
2022-09-19 08:23:45 -0300

    sprites added. distance between function

--- lua-love-course/shooting-gallery/main.lua
+++ lua-love-course/shooting-gallery/main.lua
@@ -1,37 +1,38 @@
 function love.load()
   target = {}
-  target.x = 300
-  target.y = 300
   target.radius = 50
-
-  score = 0
-  timer = 0
+  target.x = math.random(love.graphics.getWidth())
+  target.y = math.random(love.graphics.getHeight())
 
   gameFont = love.graphics.newFont(40)
+  score = 0
+  timer = 0
 end
 
-function love.update(dt)
-  
+function love.update()
+
 end
 
 function love.draw()
-  love.graphics.setColor(1, 0, 0)
-  love.graphics.circle('fill', target.x, target.y, target.radius)
-
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.setFont(gameFont)
+  love.graphics.setColor(0, 0, 0)
   love.graphics.print(score, 0, 0)
+  love.graphics.setFont(gameFont)
+
+  love.graphics.setColor(1, 0, 0)
+  love.graphics.circle("fill", target.x, target.y, 50)
 end
 
-function love.mousepressed(x, y, button, istouch, presses)
+function love.mousepressed(x, y, buton, istouch, presses)
   if button == 1 then
     local mouseToTarget = distanceBetween(x, y, target.x, target.y)
     if mouseToTarget < target.radius then
       score = score + 1
+      target.x = math.random(target.radius, love.graphics.getWidth())
+      target.y = math.random(target.radius, love.graphics.getHeight())
     end
   end
 end
 
 function distanceBetween(x1, y1, x2, y2)
-  return math.sqrt((x2 - x1)^2 + (y2 - x1)^2)
+  return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
 end
