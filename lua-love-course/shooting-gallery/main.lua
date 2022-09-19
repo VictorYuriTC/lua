e15f0fb5a5aaf57c1022edbb17a5fa3d380a3b35
Victor Yuri <victoryuritc@yahoo.com>
2022-09-19 07:20:17 -0300

    shooting gallery initial commit

---
+++ lua-love-course/shooting-gallery/main.lua
@@ -0,0 +1,12 @@
+function love.load()
+  
+end
+
+function love.update(dt)
+  
+end
+
+function love.draw()
+  love.graphics.rectangle('line', 200, 200, 80, 20)
+  love.graphics.circle('fill', 250, 250, 30)
+end
