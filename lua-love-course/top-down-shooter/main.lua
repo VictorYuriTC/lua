3eff13787081b4bc8084ca861e78cf5ec71ba593
Victor Yuri <victoryuritc@yahoo.com>
2022-09-19 20:15:01 -0300

    player movement and speed

--- lua-love-course/top-down-shooter/main.lua
+++ lua-love-course/top-down-shooter/main.lua
@@ -4,12 +4,38 @@ function love.load()
   sprites.bullet = love.graphics.newImage('sprites/bullet.png')
   sprites.player = love.graphics.newImage('sprites/player.png')
   sprites.zombie = love.graphics.newImage('sprites/zombie.png')
+
+  player = {}
+  player.x = love.graphics.getWidth() / 2
+  player.y = love.graphics.getHeight() / 2
+
+  player.speed = 180
 end
 
-function love.update()
+function love.update(dt)
+  local playerSpeed = player.speed
+  local playerX = player.x
+  local playerY = player.y
 
+  if love.keyboard.isDown('d') then
+    player.x = playerX + playerSpeed * dt
+  end
+
+  if love.keyboard.isDown('a') then
+    player.x = playerX - playerSpeed * dt
+  end
+
+  if love.keyboard.isDown('w') then
+    player.y = playerY - playerSpeed * dt
+  end
+
+  if love.keyboard.isDown('s') then
+    player.y = playerY + playerSpeed * dt
+  end
 end
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
+
+  love.graphics.draw(sprites.player, player.x, player.y)
 end
