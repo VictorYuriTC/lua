8ee8274d1aa38b2f8ae149a5d503a89cdb9958ab
Victor Yuri <victoryuritc@yahoo.com>
2022-09-19 07:44:33 -0300

    target added. mousepressed on target increase score

--- lua-love-course/shooting-gallery/main.lua
+++ lua-love-course/shooting-gallery/main.lua
@@ -1,5 +1,13 @@
 function love.load()
-  
+  target = {}
+  target.x = 300
+  target.y = 300
+  target.radius = 50
+
+  score = 0
+  timer = 0
+
+  gameFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
@@ -7,6 +15,23 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.rectangle('line', 200, 200, 80, 20)
-  love.graphics.circle('fill', 250, 250, 30)
+  love.graphics.setColor(1, 0, 0)
+  love.graphics.circle('fill', target.x, target.y, target.radius)
+
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.setFont(gameFont)
+  love.graphics.print(score, 0, 0)
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+  if button == 1 then
+    local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+    if mouseToTarget < target.radius then
+      score = score + 1
+    end
+  end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+  return math.sqrt((x2 - x1)^2 + (y2 - x1)^2)
 end
