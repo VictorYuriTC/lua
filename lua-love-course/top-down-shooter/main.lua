f2a28786560177e55fa3881d6195633fe3e1139e
Victor Yuri <victoryuritc@yahoo.com>
2022-09-19 21:14:41 -0300

    game over when zombie touches player

--- lua-love-course/top-down-shooter/main.lua
+++ lua-love-course/top-down-shooter/main.lua
@@ -40,7 +40,14 @@ function love.update(dt)
   for i,z in ipairs(zombies) do
     z.x = z.x + (math.cos(zombiePlayerAngle(z)) * z.speed * dt)
     z.y = z.y + (math.sin(zombiePlayerAngle(z)) * z.speed * dt)
+
+    if distanceBetween(z.x, z.y, player.x, player.y) < 40 then
+      for i,z in ipairs(zombies) do
+        zombies[i] = nil
+      end
+    end
   end
+
 end
 
 function love.draw()
@@ -78,4 +85,8 @@ function spawnZombie()
   zombie.y = math.random(0, love.graphics.getHeight())
   zombie.speed = 140
   table.insert(zombies, zombie)
+end
+
+function distanceBetween(x1, y1, x2, y2)
+  return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
 end
