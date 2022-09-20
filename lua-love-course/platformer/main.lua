6511d96880e2c9b8209219d30af15030e1b9fd8d
Victor Yuri <victoryuritc@yahoo.com>
2022-09-20 02:31:41 -0300

    player destryoed entering dangerZone. setFixedRotation, colission_class

--- lua-love-course/platformer/main.lua
+++ lua-love-course/platformer/main.lua
@@ -1,18 +1,27 @@
 function love.load()
   wf = require 'libraries/windfield/windfield'
-  world = wf.newWorld(0, 800)
+  world = wf.newWorld(0, 800, false)
 
-  player = world:newRectangleCollider(360, 100, 80, 80)
-  platform = world:newRectangleCollider(250, 400, 300, 100)
+  world:addCollisionClass('Platform')
+  world:addCollisionClass('Player')
+  world:addCollisionClass('Danger')
+
+  player = world:newRectangleCollider(360, 100, 80, 80, { collision_class = 'Player' })
+  player.speed = 240
+  player:setFixedRotation(true)
+
+  platform = world:newRectangleCollider(250, 400, 300, 100, { collision_class = 'Platform' })
   platform:setType('static')
+
+  dangerZone = world:newRectangleCollider(0, 550, 800, 50, { collision_class = 'Danger' })
+  dangerZone:setType('static')
 end
 
 function love.update(dt)
   world:update(dt)
 
   playerMovimentation()
-
-
+  playerPerishesWhenEnteringDangerZone()
 end
 
 function love.draw()
@@ -26,13 +35,21 @@ function love.keypressed(key)
 end
 
 function playerMovimentation()
-  local px, py = player:getPosition()
+  if player.body then
+    local px, py = player:getPosition()
 
-  if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
-    player:setX(px + 5)
+    if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
+      player:setX(px + 5)
+    end
+
+    if love.keyboard.isDown('left') or love.keyboard.isDown('a') then
+      player:setX(px - 5)
+    end
   end
+end
 
-  if love.keyboard.isDown('left') or love.keyboard.isDown('a') then
-    player:setX(px - 5)
+function playerPerishesWhenEnteringDangerZone()
+  if player:enter('Danger') then
+    player:destroy()
   end
 end
