64af52ae8ef9b4435cc840e216035dbac60e3eaa
Victor Yuri <victoryuritc@yahoo.com>
2022-09-20 18:49:59 -0300

    animations added without considering movement

--- lua-love-course/platformer/main.lua
+++ lua-love-course/platformer/main.lua
@@ -4,6 +4,14 @@ function love.load()
   sprites = {}
   sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
 
+  local grid = anim8.newGrid(614, 564, sprites
+    .playerSheet:getWidth(), sprites.playerSheet:getHeight())
+
+  animations = {}
+  animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
+  animations.jump = anim8.newAnimation(grid('1-7', 2), 0.05)
+  animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+
   wf = require 'libraries/windfield/windfield'
   world = wf.newWorld(0, 800, false)
 
@@ -12,8 +20,9 @@ function love.load()
   world:addCollisionClass('Danger')
 
   player = world:newRectangleCollider(360, 100, 80, 80, { collision_class = 'Player' })
-  player.speed = 240
   player:setFixedRotation(true)
+  player.speed = 240
+  player.animation = animations.run
 
   platform = world:newRectangleCollider(250, 400, 300, 100, { collision_class = 'Platform' })
   platform:setType('static')
@@ -27,10 +36,13 @@ function love.update(dt)
 
   playerMovimentation()
   playerPerishesWhenEnteringDangerZone()
+
+  player.animation:update(dt)
 end
 
 function love.draw()
   world:draw()
+  player.animation:draw(sprites.playerSheet, 0, 0)
 end
 
 function love.keypressed(key)
