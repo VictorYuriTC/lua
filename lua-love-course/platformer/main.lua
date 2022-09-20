ef409fc9d15ff3a129064e879ce8b45e5910e3d9
Victor Yuri <victoryuritc@yahoo.com>
2022-09-20 02:17:57 -0300

    applyLinearImpulse, getPosition, setX, setY

--- lua-love-course/platformer/main.lua
+++ lua-love-course/platformer/main.lua
@@ -9,8 +9,30 @@ end
 
 function love.update(dt)
   world:update(dt)
+
+  playerMovimentation()
+
+
 end
 
 function love.draw()
   world:draw()
+end
+
+function love.keypressed(key)
+  if key == 'up' or key == 'space' then
+    player:applyLinearImpulse(0, -5000)
+  end
+end
+
+function playerMovimentation()
+  local px, py = player:getPosition()
+
+  if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
+    player:setX(px + 5)
+  end
+
+  if love.keyboard.isDown('left') or love.keyboard.isDown('a') then
+    player:setX(px - 5)
+  end
 end
