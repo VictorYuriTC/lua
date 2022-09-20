4c472189ac6d2e4cd64f41910865c1de92480d4a
Victor Yuri <victoryuritc@yahoo.com>
2022-09-19 22:23:41 -0300

    zombies spawn randomly

--- lua-love-course/top-down-shooter/main.lua
+++ lua-love-course/top-down-shooter/main.lua
@@ -130,11 +130,29 @@ end
 
 function spawnZombie()
   local zombie = {}
-  zombie.x = math.random(0, love.graphics.getWidth())
-  zombie.y = math.random(0, love.graphics.getHeight())
+  local side = math.random(1, 4)
+
+  zombie.x = 0
+  zombie.y = 0
   zombie.speed = 140
   zombie.isDead = false
   table.insert(zombies, zombie)
+
+  local side = math.random(1, 4)
+
+  if side == 1 then
+    zombie.x = -30
+    zombie.y = math.random(0, love.graphics.getHeight())
+  elseif side == 2 then
+    zombie.x = love.graphics.getWidth() + 30
+    zombie.y = math.random(0, love.graphics.getHeight())
+  elseif side == 3 then
+    zombie.x = math.random(0, love.graphics.getWidth())
+    zombie.y = -30
+  elseif side == 4 then
+    zombie.x = math.random(0, love.graphics.getWidth())
+    zombie.y = love.graphics.getHeight() + 30
+  end
 end
 
 function spawnBullet()
