0cfd485ad35b4bbf3b1cddc79ab3a3b2642524c3
Victor Yuri <victoryuritc@yahoo.com>
2022-09-27 01:55:31 -0300

    initial commit on Gustavo Pezzi course

---
+++ lua-programming-from-scratch/myscript.lua
@@ -0,0 +1,20 @@
+myNil = nil
+myNumber = 5
+myString = 'Hello'
+function myFunction() return end
+myTable = {}
+
+print(type(myNil))
+print(type(myNumber))
+print(type(myString))
+print(type(myFunction))
+print(type(myTable))
+--CFunction
+--userdata
+
+salary = 47.445 // 2
+
+print('The value of salary is '..salary)
+
+name = 'Leeroy Jenkins'
+print('The value of name is '..name)
