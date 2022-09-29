e9200a349acf235583f5f34b982958332139a8e8
Victor Yuri <victoryuritc@yahoo.com>
2022-09-29 04:03:27 -0300

    integers and random numbers with repeat until

---
+++ lua-programming-from-scratch/activities/7-loops-and-functions/integers-from-1000-to-0.lua
@@ -0,0 +1,8 @@
+--[[ 1. Create a Lua script that displays all integer numbers between 1000 and 1, in descending order. ]]
+
+local initial_integer = 1000
+
+while initial_integer >= 1 do
+  print(initial_integer)
+  initial_integer = initial_integer - 1
+end
